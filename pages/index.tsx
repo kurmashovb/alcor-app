0a0edc40273f3fc566f680687aa2feee5edf5510
kurmashovb <kurmashovb@gmail.com>
2025-04-26 10:37:43 +0300

    Create index.tsx

---
+++ pages/index.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div style={{ display: 'flex', height: '100vh', alignItems: 'center', justifyContent: 'center', fontSize: '24px' }}>
+      Добро пожаловать в ALCOR! 🚀
+    </div>
+  )
+}
